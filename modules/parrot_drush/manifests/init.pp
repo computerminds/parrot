aa7d9e81568137ab47e75fd65cc38222a90068ad
Jakob Torp <jakob@revealit.dk>
2015-11-05 10:50:11 +0100

    Fix some bugs when installing new drush extensions

--- modules/parrot_drush/manifests/init.pp
+++ modules/parrot_drush/manifests/init.pp
@@ -12,15 +12,15 @@ class parrot_drush {
     command     => "/usr/local/bin/composer global require $parrot_drush_version",
     user        => 'vagrant',
     require     => Class['php::composer'],
-    environment => 'COMPOSER_HOME=/composer',
+    environment => 'COMPOSER_HOME=/home/vagrant',
   }
 
   # Make drush executional.
   # TODO A better solution would be to modify the .bashrc file instead of
-  # making a symlink for the drush bin.
+  # making a symlink for the drush file.
   file { '/usr/local/bin/drush':
      ensure => 'link',
-     target => '/composer/vendor/drush/drush/drush.php',
+     target => '/home/vagrant/.composer/vendor/drush/drush/drush.php',
   }
 
 }
