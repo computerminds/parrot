85184fe42b0a431c907d37497b252580e891d07a
Steven Jones <steven.jones@computerminds.co.uk>
2016-01-12 13:58:52 +0000

    Revert "Require the PHP repos stuff before we install."
    This reverts commit d22d36bccce391530b33e11cce5b11c362dd0ea4.

--- modules/parrot_php/manifests/init.pp
+++ modules/parrot_php/manifests/init.pp
@@ -6,6 +6,12 @@ class parrot_php (
 
   include '::php'
 
+  $php_packages = [
+   'php5-cgi',
+   'php5-sqlite',
+   'php5-xmlrpc',
+  ]
+
   class { [
     '::php::fpm',
     '::php::cli',
@@ -14,9 +20,8 @@ class parrot_php (
     '::php::extension::mysql',
     '::php::pear',
     '::php::dev',
-    ]:
-    require => Class["parrot_repos"],
-  }
+
+  ]: }
 
   php::fpm::config { 'parrot-settings':
     config => [
